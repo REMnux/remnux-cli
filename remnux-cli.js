8f4da3c138c2a7211683ce076a4cbbfe951a9d44
lennyzeltser <lenny@zeltser.com>
2020-07-30 22:54:10 -0400

    Switched SaltStack version from 3000 to 3001

--- remnux-cli.js
+++ remnux-cli.js
@@ -49,7 +49,7 @@ Options:
   --verbose             Display verbose logging
 `
 
-const saltstackVersion = '3000'
+const saltstackVersion = '3001'
 const pubKey = `
 -----BEGIN PGP PUBLIC KEY BLOCK-----
 Version: GnuPG

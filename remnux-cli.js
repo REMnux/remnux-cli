295cf26120879ae26b8c8ee35960ecacab92bfb8
Sean Whalen <44679+seanthegeek@users.noreply.github.com>
2023-08-24 15:09:02 -0400

    Bump saltstack version to 3006

--- remnux-cli.js
+++ remnux-cli.js
@@ -49,7 +49,7 @@ Options:
   --verbose             Display verbose logging
 `
 
-const saltstackVersion = '3004'
+const saltstackVersion = '3006'
 const pubKey = `
 -----BEGIN PGP PUBLIC KEY BLOCK-----
 Version: GnuPG
